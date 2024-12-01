aeac64af791b9914bb4c18baf20b10ef7e938740
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-01 09:03:12 +0100

    Add the beginnings of the navigation

---
+++ src/nav.jsx
@@ -0,0 +1,17 @@
+export default function Navigation() {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>Logo</li>
+                    <li>Games</li>
+                    <li>About</li>
+                    <li>Help</li>
+                    <li>Search</li>
+                    <li>Notify</li>
+                    <li>Account</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
