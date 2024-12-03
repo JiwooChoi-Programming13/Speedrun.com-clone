435ae9d55ee06a8855f903607ba7e2b5ddd04aad
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-03 21:16:49 +0100

    Use props to reduce duplication for the dropdown menu items

--- src/nav.jsx
+++ src/nav.jsx
@@ -5,6 +5,14 @@ import Notification from '../public/Notification.png'
 import Avatar from '../public/AvatarTest.jpg'
 import './nav.css'
 
+function DropdownList({text, className = "dropdown-menu-list"}) {
+    return (
+        <div className={className}>
+            <p>{text}</p>
+        </div>
+    )
+}
+
 export default function Navigation() {
     const [active, setActive] = useState(false);
     
@@ -13,62 +21,64 @@ export default function Navigation() {
     };
 
     return (
-        <>
-            <nav>
-                <ul>
-                    <li className='logo-icon'>
+        <nav>
+            <ul>
+                <li className='logo-icon'>
+                    <img
+                    src={Logo}
+                    alt="Logo"
+                    className='logo'
+                    />
+                    <h3>SPEEDRUN.COM</h3>
+                </li>
+
+                <li className='hovered'>Games</li>
+                <li className='hovered'>About</li>
+                <li className='hovered'>Help</li>
+
+                <li className='search-icon'>
+                    <label htmlFor="search-bar">
                         <img
-                        src={Logo}
-                        alt="Logo"
-                        className='logo'
+                        src={Search}
+                        alt="Search"
+                        className='search'/>
+                    </label>
+
+                    <input className='search-bar' type="text" id='search-bar' placeholder='Search...'/>
+                </li>
+
+                <li className='notification-icon'>
+                    <img src={Notification}
+                    alt="Notification"
+                    className='notification'
+                    />
+                    <p className='notification-tooltip'>Notifications</p>
+                </li>
+
+                <li className='profile-icon' onClick={handleDropdown}>
+                    <img
+                    src={Avatar}
+                    alt="Test"
+                    className='profile'/>
+                    
+                    <span>Dropdown</span>
+                    {active && (
+                    <div className='dropdown-menu'>
+                        <DropdownList
+                        text="Username"
                         />
-                        <h3>SPEEDRUN.COM</h3>
-                    </li>
-                    <li className='hovered'>Games</li>
-                    <li className='hovered'>About</li>
-                    <li className='hovered'>Help</li>
-                    <li className='search-icon'>
-                        <label htmlFor="search-bar">
-                            <img
-                            src={Search}
-                            alt="Search"
-                            className='search'/>
-                        </label>
 
-                        <input className='search-bar' type="text" id='search-bar' placeholder='Search...'/>
-                    </li>
-                    <li className='notification-icon'>
-                        <img src={Notification}
-                        alt="Notification"
-                        className='notification'
+                        <DropdownList
+                        text="Settings"
                         />
-                        <p className='notification-tooltip'>Notifications</p>
-                    </li>
-                    <li className='profile-icon' onClick={handleDropdown}>
-                        <img
-                        src={Avatar}
-                        alt="Test"
-                        className='profile'/>
-                        
-                        <span>Dropdown</span>
-                        {active && (
-                            <div className='dropdown-menu' >
-                            <div className='dropdown-menu-list'>
-                                <p>Username</p>
-                            </div>
 
-                            <div className='dropdown-menu-list'>
-                                <p>Settings</p>
-                            </div>
-
-                            <div className='dropdown-menu-list'>
-                                <p>Sign out</p>
-                            </div>
-                        </div>
-                        )}
-                    </li>
-                </ul>
-            </nav>
-        </>
+                        <DropdownList
+                        text="Sign out"
+                        />
+                    </div>
+                    )}
+                </li>
+            </ul>
+        </nav>
     )
 }
