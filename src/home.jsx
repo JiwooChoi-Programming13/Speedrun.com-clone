50363f5f0ab4a40cd2594b3922fd3f40e82270fa
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-05 15:04:05 +0100

    Move the Navigation component to a compontent folder and make the starting files for the home page

---
+++ src/home.jsx
@@ -0,0 +1,10 @@
+import Navigation from "./components/navigation/nav.jsx"
+
+export default function Home() {
+    return (
+        <>
+            <Navigation/>
+            <h1>Hello</h1>
+        </>
+    )
+}
