bd2314d5c2435ae270f6cc34daf3e47d15981d82
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-05 16:39:39 +0100

    Add 4 more games to the list

--- src/home.jsx
+++ src/home.jsx
@@ -9,7 +9,65 @@ export default function Home() {
             <div className="container">
                 <div className="games-list-container">
                     <div className="games-list">
-                        <div >
+                        
+                        <div className="game">
+                            <div className="game-content">
+                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
+                                <div className="top-info">
+                                    <p className="release-year">1996</p>
+                                </div>
+                                <div className="bottom-info">
+                                    <p>Super Mario 64</p>
+                                </div>
+                            </div>
+                            <div className="details-content">
+                                <p className="active-players">417 active players</p>
+                                <div className="platform-container">
+                                    <p className="platform">N64</p>
+                                    <p className="platform">+3</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="game">
+                            <div className="game-content">
+                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
+                                <div className="top-info">
+                                    <p className="release-year">1996</p>
+                                </div>
+                                <div className="bottom-info">
+                                    <p>Super Mario 64</p>
+                                </div>
+                            </div>
+                            <div className="details-content">
+                                <p className="active-players">417 active players</p>
+                                <div className="platform-container">
+                                    <p className="platform">N64</p>
+                                    <p className="platform">+3</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="game">
+                            <div className="game-content">
+                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
+                                <div className="top-info">
+                                    <p className="release-year">1996</p>
+                                </div>
+                                <div className="bottom-info">
+                                    <p>Super Mario 64</p>
+                                </div>
+                            </div>
+                            <div className="details-content">
+                                <p className="active-players">417 active players</p>
+                                <div className="platform-container">
+                                    <p className="platform">N64</p>
+                                    <p className="platform">+3</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="game">
                             <div className="game-content">
                             <img className="game-image" src={GameImage} alt="Super Mario 64"/>
                                 <div className="top-info">
