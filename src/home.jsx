bcc9b9e24e66a98429cb2795269bba7c8bcf0618
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-05 16:51:35 +0100

    Remove unnecessary div containers for absolute elements

--- src/home.jsx
+++ src/home.jsx
@@ -9,16 +9,57 @@ export default function Home() {
             <div className="container">
                 <div className="games-list-container">
                     <div className="games-list">
+
+                        <div className="game">
+                            <div className="game-content">
+                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
+                                <div className="release-year">1996</div>
+                                <div className="game-title">Super Mario 64</div>
+                            </div>
+                            <div className="details-content">
+                                <p className="active-players">417 active players</p>
+                                <div className="platform-container">
+                                    <p className="platform">N64</p>
+                                    <p className="platform">+3</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="game">
+                            <div className="game-content">
+                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
+                                <div className="release-year">1996</div>
+                                <div className="game-title">Super Mario 64</div>
+                            </div>
+                            <div className="details-content">
+                                <p className="active-players">417 active players</p>
+                                <div className="platform-container">
+                                    <p className="platform">N64</p>
+                                    <p className="platform">+3</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="game">
+                            <div className="game-content">
+                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
+                                <div className="release-year">1996</div>
+                                <div className="game-title">Super Mario 64</div>
+                            </div>
+                            <div className="details-content">
+                                <p className="active-players">417 active players</p>
+                                <div className="platform-container">
+                                    <p className="platform">N64</p>
+                                    <p className="platform">+3</p>
+                                </div>
+                            </div>
+                        </div>
                         
                         <div className="game">
                             <div className="game-content">
                             <img className="game-image" src={GameImage} alt="Super Mario 64"/>
-                                <div className="top-info">
-                                    <p className="release-year">1996</p>
-                                </div>
-                                <div className="bottom-info">
-                                    <p>Super Mario 64</p>
-                                </div>
+                                <div className="release-year">1996</div>
+                                <div className="game-title">Super Mario 64</div>
                             </div>
                             <div className="details-content">
                                 <p className="active-players">417 active players</p>
@@ -32,50 +73,8 @@ export default function Home() {
                         <div className="game">
                             <div className="game-content">
                             <img className="game-image" src={GameImage} alt="Super Mario 64"/>
-                                <div className="top-info">
-                                    <p className="release-year">1996</p>
-                                </div>
-                                <div className="bottom-info">
-                                    <p>Super Mario 64</p>
-                                </div>
-                            </div>
-                            <div className="details-content">
-                                <p className="active-players">417 active players</p>
-                                <div className="platform-container">
-                                    <p className="platform">N64</p>
-                                    <p className="platform">+3</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="game">
-                            <div className="game-content">
-                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
-                                <div className="top-info">
-                                    <p className="release-year">1996</p>
-                                </div>
-                                <div className="bottom-info">
-                                    <p>Super Mario 64</p>
-                                </div>
-                            </div>
-                            <div className="details-content">
-                                <p className="active-players">417 active players</p>
-                                <div className="platform-container">
-                                    <p className="platform">N64</p>
-                                    <p className="platform">+3</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="game">
-                            <div className="game-content">
-                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
-                                <div className="top-info">
-                                    <p className="release-year">1996</p>
-                                </div>
-                                <div className="bottom-info">
-                                    <p>Super Mario 64</p>
-                                </div>
+                                <div className="release-year">1996</div>
+                                <div className="game-title">Super Mario 64</div>
                             </div>
                             <div className="details-content">
                                 <p className="active-players">417 active players</p>
