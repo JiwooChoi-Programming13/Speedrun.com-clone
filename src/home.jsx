362baec94eddce5eddbda4cfb69968a6e2d2f324
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-05 15:25:13 +0100

    Add a game to the games list

--- src/home.jsx
+++ src/home.jsx
@@ -1,10 +1,23 @@
 import Navigation from "./components/navigation/nav.jsx"
+import GameImage from "./assets/Game-image.png"
+import "./home.css"
 
 export default function Home() {
     return (
         <>
             <Navigation/>
-            <h1>Hello</h1>
+            <div className="container">
+                <div className="games-list-container">
+                    <div>
+                        <div>
+                            <p>1996</p>
+                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
+                            <p>Super Mario 64</p>
+                            <p>Active players: 46</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
