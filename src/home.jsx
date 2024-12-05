d257367ce0d395e726083ac7f6ed3b8ae094e65a
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-05 15:34:46 +0100

    Have the remaining information for one game

--- src/home.jsx
+++ src/home.jsx
@@ -8,12 +8,20 @@ export default function Home() {
             <Navigation/>
             <div className="container">
                 <div className="games-list-container">
-                    <div>
-                        <div>
-                            <p>1996</p>
-                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
+                    <div className="games-list">
+                        <div className="game-content">
+                        <img className="game-image" src={GameImage} alt="Super Mario 64"/>
+                            <div className="top-info">
+                                <p>1996</p>
+                            </div>
                             <p>Super Mario 64</p>
-                            <p>Active players: 46</p>
+                            <div>
+                                <div>
+                                    <p>Active players: 46</p>
+                                </div>
+                                <p>N64</p>
+                                <p>+3</p>
+                            </div>
                         </div>
                     </div>
                 </div>
