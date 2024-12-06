9346a20e67bf4941ff502f03d7baf9691ac4ef81
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-06 09:47:34 +0100

    Render a list of data instead of hardcoded HTML

---
+++ src/games-list.jsx
@@ -0,0 +1,38 @@
+const games = [{
+    id: 0,
+    name: "Super Mario 64",
+    image: "https://cdn2.steamgriddb.com/grid/f293653ffa5c0e53453463a82401dbec.png",
+    platform: "N64",
+    releaseYear: "1996",
+    activePlayers: "417",
+}, {
+    id: 1,
+    name: "Super Mario 64",
+    image: "https://cdn2.steamgriddb.com/grid/f293653ffa5c0e53453463a82401dbec.png",
+    platform: "N64",
+    releaseYear: "1996",
+    activePlayers: "417",
+}, {
+    id: 2,
+    name: "Super Mario 64",
+    image: "https://cdn2.steamgriddb.com/grid/f293653ffa5c0e53453463a82401dbec.png",
+    platform: "N64",
+    releaseYear: "1996",
+    activePlayers: "417",
+}, {
+    id: 3,
+    name: "Super Mario 64",
+    image: "https://cdn2.steamgriddb.com/grid/f293653ffa5c0e53453463a82401dbec.png",
+    platform: "N64",
+    releaseYear: "1996",
+    activePlayers: "417",
+}, {
+    id: 4,
+    name: "Super Mario 64",
+    image: "https://cdn2.steamgriddb.com/grid/f293653ffa5c0e53453463a82401dbec.png",
+    platform: "N64",
+    releaseYear: "1996",
+    activePlayers: "417",
+}];
+
+export default games;
--- src/home.jsx
+++ src/home.jsx
@@ -1,5 +1,5 @@
 import Navigation from "./components/navigation/nav.jsx"
-import GameImage from "./assets/Game-image.png"
+import games from "./games-list.jsx"
 import "./home.css"
 
 export default function Home() {
@@ -9,81 +9,24 @@ export default function Home() {
             <div className="container">
                 <div className="games-list-container">
                     <div className="games-list">
-
-                        <div className="game">
-                            <div className="game-content">
-                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
-                                <div className="release-year">1996</div>
-                                <div className="game-title">Super Mario 64</div>
-                            </div>
-                            <div className="details-content">
-                                <p className="active-players">417 active players</p>
-                                <div className="platform-container">
-                                    <p className="platform">N64</p>
-                                    <p className="platform">+3</p>
+                        {games.map((game) => {
+                            return (
+                                <div key={game.id} className="game">
+                                <div className="game-content">
+                                <img className="game-image" src={game.image} alt={game.name}/>
+                                    <div className="release-year">{game.releaseYear}</div>
+                                    <div className="game-title">{game.name}</div>
+                                </div>
+                                <div className="details-content">
+                                    <p className="active-players">{game.activePlayers} active players</p>
+                                    <div className="platform-container">
+                                        <p className="platform">{game.platform}</p>
+                                        <p className="platform">+3</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-
-                        <div className="game">
-                            <div className="game-content">
-                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
-                                <div className="release-year">1996</div>
-                                <div className="game-title">Super Mario 64</div>
-                            </div>
-                            <div className="details-content">
-                                <p className="active-players">417 active players</p>
-                                <div className="platform-container">
-                                    <p className="platform">N64</p>
-                                    <p className="platform">+3</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="game">
-                            <div className="game-content">
-                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
-                                <div className="release-year">1996</div>
-                                <div className="game-title">Super Mario 64</div>
-                            </div>
-                            <div className="details-content">
-                                <p className="active-players">417 active players</p>
-                                <div className="platform-container">
-                                    <p className="platform">N64</p>
-                                    <p className="platform">+3</p>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div className="game">
-                            <div className="game-content">
-                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
-                                <div className="release-year">1996</div>
-                                <div className="game-title">Super Mario 64</div>
-                            </div>
-                            <div className="details-content">
-                                <p className="active-players">417 active players</p>
-                                <div className="platform-container">
-                                    <p className="platform">N64</p>
-                                    <p className="platform">+3</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="game">
-                            <div className="game-content">
-                            <img className="game-image" src={GameImage} alt="Super Mario 64"/>
-                                <div className="release-year">1996</div>
-                                <div className="game-title">Super Mario 64</div>
-                            </div>
-                            <div className="details-content">
-                                <p className="active-players">417 active players</p>
-                                <div className="platform-container">
-                                    <p className="platform">N64</p>
-                                    <p className="platform">+3</p>
-                                </div>
-                            </div>
-                        </div>
+                            )
+                        })}
                     </div>
                 </div>
             </div>
