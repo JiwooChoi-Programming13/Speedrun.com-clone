e165de05324c9497d7c3333bcd664ea80011c11b
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-07 12:15:02 +0100

    Add some more game test data

--- src/components/game-data/games-data.jsx
+++ src/components/game-data/games-data.jsx
@@ -19,6 +19,28 @@ const games = [{
     activePlayers: "417",
 }, {
     id: 2,
+    name: "Call of Duty: Black Ops 6 Zombies",
+    image: "https://cdn2.steamgriddb.com/grid/ccd427e9a45f148777fe91e6850bf8eb.png",
+    platform: "PS4",
+    otherPlatforms: [
+        "XboxOne", "PC", "XboxOneX", "PS4Pro", "XboxOneS",
+        "PS5", "XboxOneSeriesX", "XboxOneSeriesS", "Gamepass"
+    ],
+    releaseYear: "2024",
+    activePlayers: "369",
+}, {
+    id: 3,
+    name: "Shadow Generations",
+    image: "https://cdn2.steamgriddb.com/grid/be40176301531259a8a32f1c7597522f.png",
+    platform: "PS4",
+    otherPlatforms: [
+        "XboxOne", "PC", "Switch", "PS5",
+        "XboxOneSeriesX", "XboxOneSeriesS"
+    ],
+    releaseYear: "2024",
+    activePlayers: "334",
+}, {
+    id: 4,
     name: "Minecraft: Java Edition",
     image: "https://cdn2.steamgriddb.com/grid/3d91a5ece54b086486d1573a93d2f94b.jpg",
     platform: "PC",
@@ -26,7 +48,7 @@ const games = [{
     releaseYear: "2011",
     activePlayers: "253",
 }, {
-    id: 3,
+    id: 5,
     name: "Cuphead",
     image: "https://cdn2.steamgriddb.com/grid/25dcf1554f13c36b512dfe907acc77d3.png",
     platform: "PS4",
@@ -37,7 +59,26 @@ const games = [{
     releaseYear: "2017",
     activePlayers: "200",
 }, {
-    id: 4,
+    id: 6,
+    name: "UFO 50",
+    image: "https://cdn2.steamgriddb.com/grid/fda352680a5c971f3c816956676b8246.png",
+    platform: "PC",
+    otherPlatforms: [],
+    releaseYear: "2024",
+    activePlayers: "138",
+}, {
+    id: 7,
+    name: "Sonic Generations",
+    image: "https://cdn2.steamgriddb.com/grid/11a70bd53186a8f0662ca1a42faa4076.png",
+    platform: "PS4",
+    otherPlatforms: [
+        "XboxOne", "PC", "Switch", "PS5",
+        "XboxOneSeriesX", "XboxOneSeriesS"
+    ],
+    releaseYear: "2024",
+    activePlayers: "138",
+}, {
+    id: 8,
     name: "Getting over it with Bennet Foddy",
     image: "https://cdn2.steamgriddb.com/grid/797a1e5473c21f6ded58930097451edf.png",
     platform: "PC",
@@ -45,13 +86,45 @@ const games = [{
     releaseYear: "2017",
     activePlayers: "130",
 }, {
-    id: 5,
+    id: 9,
+    name: "The legend of Zelda: Echoes of Wisdom",
+    image: "https://cdn2.steamgriddb.com/grid/e488d6f4064e1584c115f2cd7bed74f0.png",
+    platform: "Switch",
+    otherPlatforms: [],
+    releaseYear: "2024",
+    activePlayers: "106",
+}, {
+    id: 10,
     name: "Granny",
     image: "https://cdn2.steamgriddb.com/grid/5788c89947da0ee5c644bb4368475cba.jpg",
     platform: "Android",
     otherPlatforms: ["IOS"],
     releaseYear: "2017",
     activePlayers: "104"
-}];
+}, {
+    id: 11,
+    name: "Super Mario Party Jamboree",
+    image: "https://cdn2.steamgriddb.com/grid/d8990f79de598c52139e95e819c86c8c.png",
+    platform: "Switch",
+    otherPlatforms: [],
+    releaseYear: "2024",
+    activePlayers: "68",
+}, {
+    id: 12,
+    name: "Silent Hill 2 (2024)",
+    image: "https://cdn2.steamgriddb.com/grid/23a8793d295f5804590d596981e50b26.png",
+    platform: "PC",
+    otherPlatforms: ["PS5"],
+    releaseYear: "2024",
+    activePlayers: "66",
+}, {
+    id: 13,
+    name: "WEBFISHING",
+    image: "https://cdn2.steamgriddb.com/grid/f528102b84fe4d49912d29ed98c254ce.png",
+    platform: "PC",
+    otherPlatforms: [],
+    releaseYear: "2024",
+    activePlayers: "138",
+}, ];
 
 export default games;
