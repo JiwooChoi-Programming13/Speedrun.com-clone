38246f27ab4429bc11462c5e7d8692fc43548af6
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-10 09:01:20 +0100

    Add the beginning files for the about page

---
+++ src/about.jsx
@@ -0,0 +1,10 @@
+import Navigation from "./components/navigation/nav.jsx"
+
+export default function About() {
+    return (
+        <>
+            <Navigation />
+            <h1>Hello world</h1>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './home.jsx'
+import About from './about.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <About />
   </StrictMode>,
 )
