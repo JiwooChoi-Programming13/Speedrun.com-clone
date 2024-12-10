feb20dc2df35314f116d37ab05c32e0fbeb3caf1
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-10 09:19:20 +0100

    Create the background for the about page

--- src/about.jsx
+++ src/about.jsx
@@ -1,10 +1,16 @@
 import Navigation from "./components/navigation/nav.jsx"
+import "./about.css"
 
 export default function About() {
     return (
         <>
             <Navigation />
-            <h1>Hello world</h1>
+            <div className="background"></div>
+            <div>
+                <div>
+                    <h1>Hello world</h1>
+                </div>
+            </div>
         </>
     )
 }
