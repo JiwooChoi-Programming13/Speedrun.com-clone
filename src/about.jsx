406377e00ad94e4a9a4a66b1df941abcf946805e
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-10 09:36:01 +0100

    Create the title of the about page

--- src/about.jsx
+++ src/about.jsx
@@ -1,4 +1,5 @@
 import Navigation from "./components/navigation/nav.jsx"
+import Logo from "./assets/Logo.png"
 import "./about.css"
 
 export default function About() {
@@ -6,9 +7,18 @@ export default function About() {
         <>
             <Navigation />
             <div className="background"></div>
-            <div>
-                <div>
-                    <h1>Hello world</h1>
+            <div className="about-container">
+                <div className="title-container">
+                    <div className="title">
+                        <img className="title-logo" src={Logo} alt="" />
+                        <h1>Speedrun.com</h1>
+                    </div>
+                    <p className="title-text">
+                        Speedrun.com (Not!) is the leading platfrom for game
+                        communities to organize around the activity of speedrunning.
+                        We host communities for thousands of games, each with
+                        their own leaderboards, rules, moderators, and particapants.
+                    </p>
                 </div>
             </div>
         </>
