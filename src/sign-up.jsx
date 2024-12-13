f2fdd7122df10cddd01e44920d245b820771c844
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-13 09:34:08 +0100

    Copy over the login page to the sign up page

--- src/sign-up.jsx
+++ src/sign-up.jsx
@@ -5,6 +5,28 @@ export default function SignUp() {
     return (
         <>
             <Navigation />
+
+            <div className="login-container">
+                <div className="login-content">
+                    <div className="login-title-container">
+                        <h3 className="login-title">Sign up to Speedrun.com (Not!)</h3>
+                    </div>
+
+                    <div className="input-container">
+                        <label htmlFor="username">Username</label>
+                        <input className="login-input" type="text" id="username"/>
+
+                        <label htmlFor="password">Password</label>
+                        <input className="login-input" type="password" id="password"/>
+                    </div>
+                    <hr />
+
+                    <div className="button-container">
+                        <button className="login-button">Log in</button>
+                        <button className="other-button">Sign up</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
