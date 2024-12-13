ccecd5c5ffcc67c944b776b6c36db9398b07384a
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-13 14:13:41 +0100

    Make a new component for the routing

---
+++ src/components/Routing/Navigation-routing.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Home from '../../home.jsx'
+import About from '../../about.jsx'
+import Login from '../../login.jsx'
+import SignUp from '../../sign-up.jsx'
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "about",
+        element: <About />,
+    },
+    {
+        path: "login",
+        element: <Login />,
+    },
+    {
+        path: "signup",
+        element: <SignUp />,
+    },
+])
--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './home.jsx'
-import About from './about.jsx'
-import Login from './login.jsx'
-import SignUp from './sign-up.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "about",
-    element: <About />,
-  },
-  {
-    path: "login",
-    element: <Login />,
-  },
-  {
-    path: "signup",
-    element: <SignUp />,
-  },
-])
+import { RouterProvider } from 'react-router-dom'
+import { router } from './components/Routing/Navigation-routing.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
