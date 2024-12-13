0b86ba363f60b96ef48c89d0be86a0e080a10c41
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-13 13:53:59 +0100

    Add the React Router to the project to be able to link pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navigation from './components/navigation/nav.jsx'
 import Home from './home.jsx'
 import About from './about.jsx'
 import Login from './login.jsx'
 import SignUp from './sign-up.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "signup",
+    element: <SignUp />,
+  },
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SignUp />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
