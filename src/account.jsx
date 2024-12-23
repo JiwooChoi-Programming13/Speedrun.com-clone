e8a0bac5f0939906c6b2a2b5b163ac80eb75afc3
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-23 09:45:46 +0100

    Make the user info container

--- src/account.jsx
+++ src/account.jsx
@@ -1,27 +1,34 @@
 import Navigation from "./components/Navigation/nav";
 import Avatar from "./assets/AvatarTest.jpg";
+import "./index.css";
 
 export default function Account() {
     return (
         <>
             <Navigation />
-
-            <div>
-                <div>
-                    <div>
+            <div className="flex justify-center mt-20">
+                <div className="bg-[#252f37] rounded-2xl">
+                    <div className="flex items-center gap-2 p-10">
                         <div>
                             <img className="rounded-full h-20 w-20" src={Avatar} alt=""/>
                         </div>
                         <div>
-                            <p>Saxophone_Guy123</p>
-                            <p>He/him</p>
-
                             <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200" width="300" height="200">
-                                    <rect width="300" height="200" fill="white" />
-                                    <circle cx="150" cy="100" r="60" fill="red" />
-                                </svg>
-                                <p>Japan</p>
+                                <h1 className="text-xl font-bold">Saxophone_Guy123</h1>
+                            </div>
+
+                            <div className="flex flex-col">
+                                <div>
+                                    <p>He/him</p>
+                                </div>
+
+                                <div className="flex items-center gap-2">
+                                    <svg className="rounded-sm" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200" width="30" height="20">
+                                        <rect width="300" height="200" fill="white" />
+                                        <circle cx="150" cy="100" r="60" fill="red" />
+                                    </svg>
+                                    <p>Japan</p>
+                                </div>
                             </div>
                         </div>
                     </div>
