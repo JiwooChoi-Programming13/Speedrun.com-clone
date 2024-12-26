02c7636d6af4d3f62f80155bc0e48bb309ef10a9
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-26 09:46:33 +0100

    Make the top section of the leaderboard page

--- src/leaderboard.jsx
+++ src/leaderboard.jsx
@@ -6,9 +6,36 @@ export default function Leaderboard() {
         <>
             <Navigation />
 
-            <div>
-                <div>
-                    <div></div>
+            <div className="flex justify-center mt-32">
+                <div className="max-w-4xl w-full">
+                    <div className="bg-[#252f37]">
+                        <div className="flex items-center gap-4 p-6">
+                            <img className="max-h-36" src="https://cdn2.steamgriddb.com/grid/f293653ffa5c0e53453463a82401dbec.png" alt="Super Mario 64" />
+                            
+                            <div className="flex flex-col gap-2">
+                                <div className="flex font-bold gap-1 items-center text-lg">
+                                    <p className="text-[#58d68d] text-2xl">Super Mario 64</p>
+                                    <p>(1996)</p>
+                                </div>
+
+                                <div className="flex flex-col gap-2">
+                                    <p>Super Mario Series</p>
+
+                                    <div className="flex gap-2">
+                                        <span className="game-platform">N64</span>
+                                        <span className="game-platform">WiiVC</span>
+                                        <span className="game-platform">WiiUVC</span>
+                                        <span className="game-platform">Switch</span>
+                                    </div>
+
+                                    <div className="flex gap-2">
+                                        <p className="bg-[#ffffff1a] cursor-pointer text-sm opacity-80 p-1 px-2 rounded">Discord</p>
+                                        <p className="bg-[#ffffff1a] cursor-pointer text-sm opacity-80 p-1 px-2 rounded">Website</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div></div>
                 </div>
             </div>
