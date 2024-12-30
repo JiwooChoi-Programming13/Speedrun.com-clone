0ba2abe49efd80c87d6d88821370f65ab1c5a126
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2024-12-30 10:20:27 +0100

    Revamp the submit section to remove unnecessary divs

--- src/submit.jsx
+++ src/submit.jsx
@@ -10,8 +10,29 @@ export default function Submit() {
                 <div className="flex flex-col gap-6 max-w-5xl w-full">
                     <div className="flex gap-4 max-[1020px]:flex-col">
                         <div className="flex flex-col max-w-5xl w-full">
-                            <div className="bg-[#2c453f99] flex flex-col flex-no gap-3 p-2 rounded">
-                                
+                            <div className="bg-[#2c453f99] flex flex-col flex-no gap-5 rounded">
+                                <div>
+                                    <div className="p-4">
+                                        <p>SUBMIT RUN</p>
+                                        <p>Categorization</p>
+                                        <p>
+                                            These values determine which leaderboards the run will be
+                                            ranked on.
+                                        </p>
+                                    </div>
+
+                                    <div className="flex flex-col mb-5 px-4 py-2">
+                                        {/* 
+                                        FIXME: Have a platforms list to filter out a list of
+                                        specific platform
+                                        */}
+                                        <h3>Platform</h3>
+                                        <select className="options w-full">
+                                            <option value="120 Star">120 Star</option>
+                                        </select>
+                                    </div>
+                                    <hr className="opacity-15"/>
+                                </div>
                             </div>
                         </div>
 
