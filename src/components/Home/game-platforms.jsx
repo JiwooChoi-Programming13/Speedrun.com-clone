1c69b302bd3d9ee5ed0265af6d21e7826d9b5f32
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2025-01-02 12:31:06 +0100

    Add the context to the game-platforms component

--- src/components/Home/game-platforms.jsx
+++ src/components/Home/game-platforms.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import platforms from "./platforms-data.jsx";
+import { platformContext } from "./platforms-context.jsx";
 
 export default function Platform() {
+    const [platform, setPlatform] = useState(platformContext);
+    
     return (
         <>
             <div>
                 <h3>Platform</h3>
-                <select className="options">
+                <select
+                className="options"
+                value={platform}
+                onChange={(e) => setPlatform(e.target.value)}>
                     <option value="Any Platform">Any Platform</option>
                 {platforms.map((platform) => 
                     <option key={platform.id} value={platform.name}>{platform.name}</option>
