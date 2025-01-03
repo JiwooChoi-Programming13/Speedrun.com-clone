713d69946870fb5ec2f3c06fa463cba9617f892f
JiwooChoi-Programming13 <jiwooch13@gmail.com>
2025-01-03 18:25:08 +0100

    Change the code to only have useState and not context

--- src/components/Home/platforms-filter.jsx
+++ src/components/Home/platforms-filter.jsx
@@ -3,19 +3,24 @@ import Platform from "./game-platforms.jsx";
 import GamesList from "./games-list-display.jsx";
 import games from "./games-data.jsx";
 
-export function PlatformProvider() {
-    const [platform, setPlatform] = useState("Any platform");
+export function PlatformFilter() {
+    const [platforms, setPlatforms] = useState("Any platform");
+    
+    const handlePlatform = (platform) => {
+        setPlatforms(platform);
+    }
 
-    const filteredGames =
-    platform === "Any platform"
-    ? games : games.filter(
-        (game) => game.platform === platform ||
-        game.otherPlatforms.includes(platform)
-    );
+    const filteredGames = platforms === "Any platform"
+    ? games
+    : games.filter((game) => 
+        game.platform === platforms || game.otherPlatforms.includes(platforms)
+    )
 
     return (
-        <platformContext.Provider value={{ platform, setPlatform, filteredGames }}>
-            {children}
-        </platformContext.Provider>
+        <div>
+            <Platform>
+                <GamesList></GamesList>
+            </Platform>
+        </div>
     )
 }
